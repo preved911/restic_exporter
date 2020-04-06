993f5a2a47ff30c0f5ddfe08fd6e26a89d89a79d
Ildar Valiullin <preved.911@gmail.com>
2020-04-06 22:04:16 +0300

    initial commit

---
+++ go.mod
@@ -0,0 +1,8 @@
+module main
+
+go 1.13
+
+require (
+	github.com/joho/godotenv v1.3.0
+	github.com/prometheus/client_golang v1.5.1
+)
---
+++ main.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"os/exec"
+	"strings"
+	"time"
+
+	"github.com/joho/godotenv"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+)
+
+var (
+	resticCheckExitCode = prometheus.NewGaugeVec(
+		prometheus.GaugeOpts{
+			Name: "restic_check_exit_code",
+			Help: "restic check command exit code result.",
+		},
+		[]string{"prefix"},
+	)
+)
+
+func resticCheck() {
+	// override restic executable path
+	resticExecPath := os.Getenv("RESTIC_BINARY_PATH")
+	if resticExecPath == "" {
+		resticExecPath = "/usr/local/bin/restic"
+	}
+
+	godotenv.Load("/etc/default/restic")
+	godotenv.Load("/etc/default/restic_exporter")
+
+	prefixes := strings.Split(os.Getenv("RESTIC_CHECK_PREFIXES"), ",")
+
+	go func() {
+		for {
+			for _, prefix := range prefixes {
+				os.Setenv(
+					"RESTIC_REPOSITORY",
+					fmt.Sprintf("%s/%s", os.Getenv("RESTIC_BUCKET"), prefix))
+
+				cmd := exec.Command(resticExecPath)
+
+				// 5 attempts before failed state return
+				for i := 0; i < 5; i++ {
+					err := cmd.Run()
+					if err != nil {
+						log.Printf("check failed: %s\n", err)
+					} else {
+						resticCheckExitCode.WithLabelValues(prefix).Set(0)
+						break
+					}
+
+					if i < 4 {
+						time.Sleep(5 * time.Second)
+					} else {
+						resticCheckExitCode.WithLabelValues(prefix).Set(1)
+					}
+				}
+			}
+
+			time.Sleep(30 * time.Second)
+		}
+	}()
+}
+
+func init() {
+	prometheus.MustRegister(resticCheckExitCode)
+}
+
+func main() {
+	resticCheck()
+
+	http.Handle("/metrics", promhttp.Handler())
+	log.Println("exporter started")
+	http.ListenAndServe(":9707", nil)
+}
